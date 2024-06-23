133de592c67f35f6b0218ea5aa7a7134220730cd
Zylan <126135306+Zippland@users.noreply.github.com>
2024-06-24 01:49:36 +1000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Zippland', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个被计算机耽误的艺术生🎨', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.zihanjian.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客','Zylan','Zippland','zihan','zihan jian' // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'zihan.jian@outlook.com', // 邮箱地址 例如mail@tangly1024.com
